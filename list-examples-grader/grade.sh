d84e4431c163f7889ffe160aacb1b63ef426cedb
Benjamin M Miller <cs15lfa23gq@ieng6-203.ucsd.edu>
2023-11-27 16:46:59 -0800

    Fixed bugs

--- list-examples-grader/grade.sh
+++ list-examples-grader/grade.sh
@@ -25,8 +25,8 @@ cp -r lib grading-area
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
 cd grading-area
-javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
-java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > output.txt
+javac -cp ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar" *.java
+java -cp ".:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > output.txt
 echo "Tests ran sucessfully. The got the following output:"
 grep -h "Fail" output.txt
 grep -h "OK" output.txt
